28be6433117b2be8d02d083e5707eda0857f6f68
CHoudrouge4 <hah51@mail.aub.edu>
2017-10-22 23:00:55 +0200

    construction

--- kd_tree.h
+++ kd_tree.h
@@ -8,19 +8,20 @@ typedef std::vector<double> point;
 struct node {
 	double r; 
 	std::vector<int> pts;
-	std::unique_ptr<node> left; // change unique to shared
-	std::unique_ptr<node> right;
-	std::unique_ptr<node> parent;
+	std::shared_ptr<node> left; // change unique to shared
+	std::shared_ptr<node> right;
+	std::shared_ptr<node> parent;
 };
 
 class kd_tree {	
 private: 
-	std::unique_ptr<node> root = std::make_unique<node>();
-	static std::vector<point> points;
+	int dimension;
+	std::shared_ptr<node> root = std::make_shared<node>();
+	std::vector<point> points;
 	double dist(const point p, const point q);
 	// v is not sorted 
 	double median(std::vector<double> &v);  
-	void   construct(std::unique_ptr<node> current, int index);
+	void   construct(std::shared_ptr<node> &current, int index);
 	void   read_point(const std::string file_name);
 public:
 	kd_tree() {}
